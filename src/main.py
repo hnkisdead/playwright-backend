99760137de9c486df1b6e89d9183fca2f74411f0
Василий Субботин <vs@smena.space>
2021-07-30 15:29:03 +0500

    Add CORS

--- src/main.py
+++ src/main.py
@@ -2,9 +2,21 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:1234",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
